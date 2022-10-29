d85f6139da1e0c8f58c4419c4c7dc95eef222b4f
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-10-28 17:19:22 -0700

    showing services first step

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,6 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import GeneralContext from "../contexts/GeneralContext";
 import './Services.scss';
@@ -6,14 +8,47 @@ import './Services.scss';
 const Services = () => {
 
   const { serviceGroups, services } = useContext(GeneralContext);
+  const [visGroup, setVisGroup] = useState([]);
+  
+  useEffect(() => {
+    setVisGroup(serviceGroups.map(row => {
+      return ({...row, vis: false})
+    }))
+  }, [serviceGroups]);
 
-  const groupsArray = serviceGroups.map(row => {
+  // function to change visibilty of service box!
+  const groupClicked = function (id) {
+    setVisGroup(visGroup.map(row => {
+      if (row.id === id) row.vis = !row.vis;
+      return row;
+    }));
+  }
+
+  const groupsArray = visGroup.map(row => {
+
+    const filteredService = services.filter(service => service.groupid === row.id)
+      .map(xService => {
+        // service-box
+        return (
+          <div className="service-box" style={{display: row.vis ? 'flex' : 'none' }}>
+            <span>{xService.service}</span>
+          </div>
+        )
+      })
+
     return (
-      <div className="service-group">
-        <span>{row.group}</span>
+      <div className="service-group" key={row.id}>
+        <div className="service-group-box" onClick={() => groupClicked(row.id)}>
+          <span className="arrow"><FontAwesomeIcon icon="fa-solid fa-chevron-down" /></span>
+          <span className="service-group-line">{row.group}</span>
+        </div>
+        <div className="service-box-main">{filteredService}</div>
       </div>
-    )
-  })
+    );
+  });
+
+  console.log(visGroup);
+
   return (
     (serviceGroups) && (
       <div className=''>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
