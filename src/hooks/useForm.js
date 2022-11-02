a7d9df94eee3c09e3949c7a2ece5ea10cb6e86d4
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-11-02 13:56:53 -0700

    change service and stylists base on validation - update hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function useForm(baseFormData, action) {
   const [formData, setFormData] = useState(baseFormData);
 
+  useEffect(() => {
+    if (formData.stylists.length !== 0) {
+
+      const myGroup = formData.service.groupid;
+      const myStylists = formData.stylists.filter(stylist => stylist.skills.indexOf(myGroup) > -1)
+      setFormData(formData => ({ ...formData, stylists: myStylists }));
+    }
+  }, [formData.service])
+
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData(formData => ({ ...formData, [name]: value }));
   };
 
   const handleChangeStylists = (event) => {
-    setFormData({ ...formData, stylists: event.target.value })
+    setFormData(formData => ({ ...formData, stylists: event.target.value }))
   };
 
   const handleSubmit = (event) => {
