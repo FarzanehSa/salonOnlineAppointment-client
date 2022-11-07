3b1ca6a082b8cd1c822b99bd536ca2e04cfbbe3a
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-11-06 17:31:06 -0800

    week fix bug

--- src/components/WeeklyCalender.jsx
+++ src/components/WeeklyCalender.jsx
@@ -4,17 +4,21 @@ import GeneralContext from "../contexts/GeneralContext";
 
 import './WeeklyCalender.scss';
 
-const WeeklyCalender = ({weekInfo, weekNum, dayClicked, today}) => {
+const WeeklyCalender = ({weekInfo, weekNum, dayClicked, monthName}) => {
+
+  const now = new Date();
+  now.setHours(0, 0, 0, 0);
 
   const weekArray = weekInfo.map(row => {
 
     let myClass = "";
-    if (row.select) myClass = "weekly-calender-day-box-selected";
+    if (row.select) myClass += " weekly-calender-day-box-selected ";
+    if (row.firstDay) myClass += " weekly-calender-first-day "
 
     return (
-      <button className={`weekly-calender-day-box ${myClass}`} key={row.name}  onClick={() => dayClicked(row.date)} disabled={row.date < today}>
+      <button className={`weekly-calender-day-box ${myClass}`} key={row.name}  onClick={() => dayClicked(row.fullDate)} disabled={row.fullDate < now}>
         <span className="weekly-calender-day-name">{row.name.slice(0, 3)}</span>
-        <span className="weekly-calender-day-date">{row.date.split('/')[1]}</span>
+        <span className="weekly-calender-day-date">{row.fullDate.getDate().toString()}</span>
       </button>
     )
   })
@@ -22,7 +26,7 @@ const WeeklyCalender = ({weekInfo, weekNum, dayClicked, today}) => {
   return (
     <div className='weekly-calender'>
       <div className="weekly-calender-month-box">
-        <span className="weekly-calender-month">{weekInfo[0].month}, </span>
+        <span className="weekly-calender-month">{monthName}, </span>
         {(weekNum === 0 ) && <span className="weekly-calender-week-num">This Week</span>}
         {(weekNum === 1 ) && <span className="weekly-calender-week-num">Next Week</span>}
         {(weekNum !== 0 &&  weekNum !== 1) && <span className="weekly-calender-week-num">In {weekNum} weeks.</span>}
